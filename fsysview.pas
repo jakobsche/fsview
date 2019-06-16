5e11203998ada2bb654516d071d00d972078063f
jakobsche <messages@jakobsche.de>
2019-06-16 11:55:00 +0200

    update

--- fsysview.pas
+++ fsysview.pas
@@ -52,6 +52,14 @@ procedure Register;
 
 implementation
 
+uses Patch;
+
+type
+  TFileDescription = class(TObject)
+  private
+    FSR: TSearchRec;
+  end;
+
 procedure Register;
 begin
   RegisterComponents('Misc',[TFileSystemView]);
@@ -74,9 +82,24 @@ end;
 { TFileSystemView }
 
 constructor TFileSystemView.Create(AOwner: TComponent);
+var
+  SR: TSearchRec;
+  R: Longint;
+  Root, TN: TTreeNode;
+  FD: TFileDescription;
 begin
   inherited Create(AOwner);
-
+  R := FindFirst('/', faDirectory, SR);
+  if R = 0 then begin
+    FD := TFileDescription.Create;
+    Move(SR, FD.FSR, SizeOf(SR));
+    Root := FSTreeView.Items.AddObjectFirst(nil, '/', FD);
+  end;
+  FindClose(SR);
+  R := FindFirst('/*', faDirectory, SR);
+  Directory := GetEnvironmentVariable('HOME');
+  TN := FSTreeView.Items.FindNodeWithTextPath('Directory');
+  if TN = nil then TN := FSTreeView.Items.AddObject(Root, Directory, FD);
 end;
 
 procedure TFileSystemView.Loaded;
@@ -89,7 +112,8 @@ function TFileSystemView.GetFileListView: TFileListView;
 begin
   if not Assigned(FFileListView) then begin
     FFileListView := TFileListView.Create(Self);
-    FFileListView.Parent := Sides[0];
+    FFileListView.Parent := Sides[1];
+    FFileListView.Align := alClient
   end;
   Result := FFileListView
 end;
@@ -98,15 +122,40 @@ function TFileSystemView.GetFSTreeView: TFileSystemTreeView;
 begin
   if not Assigned(FFSTreeView) then begin
     FFSTreeView := TFileSystemTreeView.Create(Self);
-    FFSTreeView.Parent := Sides[1];
+    FFSTreeView.Parent := Sides[0];
+    FSTreeView.Align := alClient
   end;
   Result := FFSTreeView
 end;
 
 procedure TFileSystemView.SetDirectory(AValue: string);
+var
+  R: Longint;
+  SR: TSearchRec;
+  FD: TFileDescription;
+  TN: TTreeNode;
 begin
-  {...} {Baumansicht und Dateilistenansicht für den Knoten AValue erstellen}
-  FDirectory := AValue
+  if not False {(csUpdating in ComponentState)} then begin
+    {Updating;}
+    {R := FindFirst(Directory, faDirectory, SR);
+    if R := 0 then begin
+      TN := TTreeNode.Create(FSTreeView.Items);
+      TN.
+      FSTreeView.Items.Add(;
+    end;
+    FindClose(SR);}
+    R := FindFirst(BuildFileName(Directory,  '*'), faDirectory, SR);
+    while R = 0 do begin
+      FD := TFileDescription.Create;
+      Move(SR, FD.FSR, SizeOf(SR));
+      FileListView.AddItem(SR.Name, FD);
+      Application.ProcessMessages;
+      R := FindNext(SR)
+    end;
+    FindClose(SR);
+    {Updated; }
+  end;
+  FDirectory := AValue;
 end;
 
 end.
